5dff165682f54f988784ec5072f5032a9b649537
Aloïs DENIEL <alois.deniel@gmail.com>
2025-05-02 15:46:23 +0200

    Fixed typo.

--- color.go
+++ color.go
@@ -76,7 +76,7 @@ func (color *Color) Encode(writer *bufio.Writer) error {
 	if err != nil {
 		return err
 	}
-	err = writeFloat64(writer, color.Alpha))
+	err = writeFloat64(writer, color.Alpha)
 	if err != nil {
 		return err
 	}
